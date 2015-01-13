a9dee45eb8ac87d8c48ded4e65ea56a0a8030542
naleite <naleite.nuaa@gmail.com>
2015-01-13 21:58:02 +0100

    first commit

---
+++ chrome/content/main.js
@@ -0,0 +1,8 @@
+
+function showMore() {  
+  document.getElementById("more-text").hidden = false;
+}
+
+function openWindow() {
+  window.open("chrome://browser/content/places/places.xul", "bmarks", "chrome,width=600,height=300");    
+}
